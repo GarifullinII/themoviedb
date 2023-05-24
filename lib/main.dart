224484f918438de7e84881ae778a7b7f85fb1c05
GarifullinII <ildaridrisovich@gmail.com>
2023-05-24 16:52:31 +0400

    add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:themoviedb/theme/app_colors.dart';
-import 'package:themoviedb/widgets/auth/auth_model.dart';
 import 'package:themoviedb/widgets/auth/auth_widget.dart';
-import 'package:themoviedb/widgets/main_screen/main_screen_widget.dart';
-import 'package:themoviedb/widgets/movie_details/movie_details_widget.dart';
+import 'package:themoviedb/widgets/navigation/main_navigation.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static final mainNavigation = MainNavigation();
+
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
@@ -27,31 +27,9 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: const AuthWidget(),
-      initialRoute: '/auth',
-      routes: {
-        '/auth': (context) => AuthProvider(
-              model: AuthModel(),
-              child: const AuthWidget(),
-            ),
-        '/main_screen': (context) => const MainScreenWidget(),
-        '/main_screen/movie_details': (context) {
-          final arguments = ModalRoute.of(context)?.settings.arguments;
-          if (arguments is int) {
-            return MovieDetailsWidget(movieId: arguments);
-          } else {
-            return const MovieDetailsWidget(movieId: 0);
-          }
-        },
-      },
-      onGenerateRoute: (RouteSettings settings) {
-        return MaterialPageRoute(builder: (context) {
-          return const Scaffold(
-            body: Center(
-              child: Text('Navigation error'),
-            ),
-          );
-        });
-      },
+      initialRoute: mainNavigation.initialRoute(false),
+      routes: mainNavigation.routes,
+      onGenerateRoute: mainNavigation.onGenerateRoute,
     );
   }
 }
